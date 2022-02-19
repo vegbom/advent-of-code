f8694d294342f56c413cba08b1c94e77dcd13ed7
vegbom <77563873+vegbom@users.noreply.github.com>
2022-02-19 14:46:07 -0500

    wip

--- day16/day16.go
+++ day16/day16.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"encoding/hex"
+	"fmt"
+)
+
 func main() {
+	decode("D2FE28")
+}
+
+func decode(in string) {
+
+	data, err := hex.DecodeString(in)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("% x", data)
+
+	getBit := func(loc int, sz int) (result uint64) {
+		byteNum := loc / 8
+	}
 
 }
