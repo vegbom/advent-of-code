0dd93accbf6466fbb6bbbeb614a27cf7503a7600
Rickey <vegbom@users.noreply.github.com>
2022-02-23 21:27:33 -0500

    omg it actually kind of works

--- day19/day19.go
+++ day19/day19.go
@@ -15,14 +15,162 @@ type Coord2D struct {
 	y int
 }
 
+type ScannerPair struct {
+	s1 int
+	s2 int
+}
+
+type Coord3D struct {
+	x int
+	y int
+	z int
+}
+
 type Scanner2D struct {
-	id      int
-	beacons []Coord2D
+	id         int
+	beaconsAbs []Coord2D
+	beaconsRel [][]Coord2D
 }
 
 func main() {
 	list := Loader("2dtest.txt")
-	fmt.Printf("%v", list)
+	fmt.Printf("%v\n", list)
+
+	overlapBeacons := make(map[ScannerPair][]int, 0)
+	for i := 0; i < (len(list) - 1); i++ {
+		s1 := list[i]
+
+		// Generate relative coordinates
+		s1.beaconsRel = getRelativeCoords2D(s1.beaconsAbs)
+
+		for j := i + 1; j < len(list); j++ {
+			s2 := list[j]
+			if i == j {
+				continue
+			}
+
+			// Rotate through all the options
+			for rotation := -3; rotation <= 3; rotation++ {
+				// Generate relative coordinates
+				s2.beaconsRel = getRelativeCoords2D(rotate2D(s2.beaconsAbs, rotation))
+
+				// Count how many same for each of the beacons and if it exceeds threashold
+				// If it does, then record the beacon ID?
+				THREASHOLD := 2
+				overlap := getOverlapBeaconIds(s1.beaconsRel, s2.beaconsRel, THREASHOLD)
+				if len(overlap) > THREASHOLD {
+					fmt.Printf("Beacon %d and %d in rotation %d has %d matching\n", i, j, rotation, len(overlap))
+					// found the matching orientation
+					overlapBeacons[ScannerPair{i, j}] = overlap
+					break
+					// break out TODO CHECK IF THIS IS TRUE? It might not be?
+				}
+			}
+		}
+	}
+
+	// try to match
+	// n := rotate2D(list[0], 0, false)
+	// fmt.Printf("%v\n", n)
+	// n = rotate2D(n, 1, false)
+	// fmt.Printf("%v\n", n)
+	// n = rotate2D(n, 3, false)
+	// fmt.Printf("%v\n", n)
+	// n = rotate2D(n, 1, false)
+	// n = rotate2D(n, 1, true)
+
+	// fmt.Printf("Relatives: %v\n", getRelativeCoords2D(n.beacons))
+	// fmt.Printf("Relatives: %v\n", getRelativeCoords2D(list[1].beacons))
+
+}
+
+func getOverlapBeaconIds(list1 [][]Coord2D, list2 [][]Coord2D, threashold int) (beaconIds []int) {
+	beaconIds = make([]int, 0)
+	for i, v1 := range list1 {
+		for _, v2 := range list2 {
+			if getNumOverlapLinks(v1, v2) >= threashold {
+				beaconIds = append(beaconIds, i)
+			}
+		}
+	}
+	return beaconIds
+}
+
+func getNumOverlapLinks(beacon1 []Coord2D, beacon2 []Coord2D) (count int) {
+	for _, link1 := range beacon1 {
+		for _, link2 := range beacon2 {
+			if link1 == link2 {
+				count++
+			}
+		}
+	}
+	return count
+}
+
+func getRelativeCoords2D(beaconsAbs []Coord2D) [][]Coord2D {
+	relCoords := make([][]Coord2D, 0)
+	for _, b1 := range beaconsAbs {
+		currentBeacon := make([]Coord2D, 0)
+		for _, b2 := range beaconsAbs {
+			currentBeacon = append(currentBeacon, Coord2D{b2.x - b1.x, b2.y - b1.y})
+		}
+		relCoords = append(relCoords, currentBeacon)
+	}
+
+	return relCoords
+}
+
+func getRelativeCoords(beacons []Coord3D) [][]Coord3D {
+	relCoords := make([][]Coord3D, 0)
+	for _, b1 := range beacons {
+		currentBeacon := make([]Coord3D, 0)
+		for _, b2 := range beacons {
+			currentBeacon = append(currentBeacon, Coord3D{b2.x - b1.x, b2.y - b1.y, b2.z - b1.z})
+		}
+		relCoords = append(relCoords, currentBeacon)
+	}
+
+	return relCoords
+}
+
+func rotate2D(beacons []Coord2D, orientation int) []Coord2D {
+	// Define orientation as follows:
+	// 0 = 0deg, 1 = 90deg CW, 2 = 180deg CW, 3 = 270deg CW
+	// If negative, means it is flipped
+
+	flip := false
+	if orientation < 0 {
+		flip = true
+		orientation *= -1
+	}
+
+	// Rotation Matrix
+	r := [2][2]int{
+		{1, 0},
+		{0, 1},
+	}
+	switch orientation {
+	case 1:
+		r = [2][2]int{{0, -1}, {1, 0}}
+	case 2:
+		r = [2][2]int{{-1, 0}, {0, -1}}
+	case 3:
+		r = [2][2]int{{0, 1}, {-1, 0}}
+	}
+
+	// Multiply matrix
+	newCoords := make([]Coord2D, 0)
+	for _, v := range beacons {
+		xNew := r[0][0]*v.x + r[0][1]*v.y
+		yNew := r[1][0]*v.x + r[1][1]*v.y
+		if flip {
+			xNew *= -1
+			yNew *= -1
+		}
+		newCoords = append(newCoords, Coord2D{xNew, yNew})
+	}
+
+	return newCoords
 }
 
 func Loader(filename string) []Scanner2D {
@@ -52,13 +200,12 @@ func Loader(filename string) []Scanner2D {
 		if scanHeader {
 			fmt.Printf("INSTR: %v\n", goScanner.Text())
 			matches := headerExp.FindStringSubmatch(goScanner.Text())
-			// instrs = append(instrs, Instruction{matches[r.SubexpIndex("dir")], MustStringToInt(matches[r.SubexpIndex("pos")])})
 			currentScanner.id = MustStringToInt(matches[headerExp.SubexpIndex("id")])
-			currentScanner.beacons = make([]Coord2D, 0)
+			currentScanner.beaconsAbs = make([]Coord2D, 0)
 			scanHeader = false
 		} else {
 			coordsString := strings.Split(goScanner.Text(), ",")
-			currentScanner.beacons = append(currentScanner.beacons, Coord2D{MustStringToInt(coordsString[0]), MustStringToInt(coordsString[1])})
+			currentScanner.beaconsAbs = append(currentScanner.beaconsAbs, Coord2D{MustStringToInt(coordsString[0]), MustStringToInt(coordsString[1])})
 		}
 	}
 	result = append(result, currentScanner)
